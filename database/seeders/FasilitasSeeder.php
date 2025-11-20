ba50a90f2c9ce7fe9dc72725b0f4e3f51c7316c2
Wandhy <wandhysyahputra127@gmail.com>
2025-11-20 23:09:42 +0700

    tambahan fasilitas

--- database/seeders/FasilitasSeeder.php
+++ database/seeders/FasilitasSeeder.php
@@ -10,9 +10,24 @@ class FasilitasSeeder extends Seeder
 {
     public function run(): void
     {
-        Fasilitas::create(['nama_fasilitas' => 'Jalan Rusak']);
-        Fasilitas::create(['nama_fasilitas' => 'Lampu Jalan Mati']);
-        Fasilitas::create(['nama_fasilitas' => 'Saluran Air Tersumbat']);
-        Fasilitas::create(['nama_fasilitas' => 'Tempat Sampah Umum']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Lab Multimedia']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Lab Jarkom']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Lab Aplikasi']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Kelas A 3.1']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Kelas A 3.2']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Kelas A 3.3']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Kelas A 3.4']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Kelas A 3.5']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang BBA']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Dosen']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Prodi']);
+        Fasilitas::create(['nama_fasilitas' => 'Ruang Perpustakaan']);
+        Fasilitas::create(['nama_fasilitas' => 'Tempat Parkir']);
+        Fasilitas::create(['nama_fasilitas' => 'Kantin']);
+        Fasilitas::create(['nama_fasilitas' => 'Koneksi Jaringan Wifi']);
+        Fasilitas::create(['nama_fasilitas' => 'Lingkungan Kampus']);
+        Fasilitas::create(['nama_fasilitas' => 'Toilet Umum']);
+        Fasilitas::create(['nama_fasilitas' => 'Mushola']);
+        Fasilitas::create(['nama_fasilitas' => 'Bangunan Gedung']);
     }
 }
